60b6f5ffdc1636b62c878a81765b70042af4193f
Seungwoo Oh <sw6016@hanmail.net>
2023-05-03 22:46:50 +0900

    Implement main entry point
    실행될 메인 로직 작성하고 정렬 알고리즘 사용
    테스트는 실행 결과만 관측하고 있음, 검증 코드는 없음

---
+++ src/main/java/com/fc/javaoop/Main.java
@@ -0,0 +1,14 @@
+package com.fc.javaoop;
+
+import com.fc.javaoop.logic.BubbleSort;
+
+import java.util.Arrays;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        BubbleSort<String> sort = new BubbleSort<>();
+            System.out.println("[result]" + sort.sort(Arrays.asList(args)));
+        }
+    }
