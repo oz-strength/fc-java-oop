2523d3782bb281f484755c7caf4b3b97db10fd4b
Seungwoo Oh <sw6016@hanmail.net>
2023-05-05 21:09:47 +0900

    Update #3 - Main
    변수 클래스를 인터페이스로 변경 
    #3은 여기서 마무리.
    스프링으로 넘어감.

--- src/main/java/com/fc/javaoop/Main.java
+++ src/main/java/com/fc/javaoop/Main.java
@@ -1,6 +1,8 @@
 package com.fc.javaoop;
 
 import com.fc.javaoop.logic.BubbleSort;
+import com.fc.javaoop.logic.JavaSort;
+import com.fc.javaoop.logic.Sort;
 
 import java.util.Arrays;
 
@@ -8,7 +10,7 @@ import java.util.Arrays;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> sort = new BubbleSort<>();
+        Sort<String> sort = new JavaSort<>();
             System.out.println("[result]" + sort.sort(Arrays.asList(args)));
         }
     }
